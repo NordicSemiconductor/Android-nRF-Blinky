8070cccdb9f2c0f575035bf5f1022829f1e6a1c5
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-09-21 14:49:22 +0200

    nRF Blinky as scanner title

--- scanner/src/main/java/no/nordicsemi/android/blinky/scanner/ScannerScreen.kt
+++ scanner/src/main/java/no/nordicsemi/android/blinky/scanner/ScannerScreen.kt
@@ -3,6 +3,7 @@ package no.nordicsemi.android.blinky.scanner
 import android.bluetooth.BluetoothDevice
 import android.os.ParcelUuid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import no.nordicsemi.android.common.ui.scanner.DeviceSelected
 import no.nordicsemi.android.common.ui.scanner.ScannerScreen
 import java.util.*
@@ -12,8 +13,9 @@ fun BlinkyScanner(
     onDeviceSelected: (BluetoothDevice) -> Unit,
 ) {
     ScannerScreen(
+        title = stringResource(id = R.string.scanner_title),
         uuid = ParcelUuid(UUID.fromString("00001523-1212-efde-1523-785feabcd123")), // TODO externalize
-        allowBack = false,
+        cancellable = false,
         onResult = { result ->
             when (result) {
                 is DeviceSelected -> onDeviceSelected(result.device.device)
