54eb81a3a8842c2f084254632233747cf6ba9037
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-09-27 14:24:27 +0200

    Planting DebugTree

--- app/src/main/java/no/nordicsemi/android/blinky/HiltApplication.kt
+++ app/src/main/java/no/nordicsemi/android/blinky/HiltApplication.kt
@@ -2,6 +2,15 @@ package no.nordicsemi.android.blinky
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class HiltApplication : Application()
+class HiltApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialize Timber. By default, the library will log to the Android logcat.
+        Timber.plant(Timber.DebugTree())
+    }
+}
