04fee77cdedec4c6cc593d908bebb283e9559bbd
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-10-10 12:06:53 +0200

    Imports organized

--- blinky/ble/src/main/java/no/nordicsemi/android/blinky/ble/BlinkyManager.kt
+++ blinky/ble/src/main/java/no/nordicsemi/android/blinky/ble/BlinkyManager.kt
@@ -11,8 +11,11 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import no.nordicsemi.android.ble.BleManager
-import no.nordicsemi.android.ble.ktx.*
+import no.nordicsemi.android.ble.ktx.asValidResponseFlow
+import no.nordicsemi.android.ble.ktx.getCharacteristic
 import no.nordicsemi.android.ble.ktx.state.ConnectionState
+import no.nordicsemi.android.ble.ktx.stateAsFlow
+import no.nordicsemi.android.ble.ktx.suspend
 import no.nordicsemi.android.blinky.ble.data.ButtonCallback
 import no.nordicsemi.android.blinky.ble.data.ButtonState
 import no.nordicsemi.android.blinky.ble.data.LedCallback
