3a09b45d7dcc14911f7e83441af979c4c46a4f00
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-10-10 14:35:47 +0200

    Strings moved around

--- scanner/src/main/java/no/nordicsemi/android/blinky/scanner/view/ScannerScreen.kt
+++ scanner/src/main/java/no/nordicsemi/android/blinky/scanner/view/ScannerScreen.kt
@@ -4,8 +4,8 @@ import android.bluetooth.BluetoothDevice
 import android.os.ParcelUuid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import no.nordicsemi.android.blinky.scanner.R
 import no.nordicsemi.android.blinky.spec.BlinkySpec
-import no.nordicsemi.android.blinky.spec.R
 import no.nordicsemi.android.common.ui.scanner.DeviceSelected
 import no.nordicsemi.android.common.ui.scanner.ScannerScreen
 
@@ -14,7 +14,7 @@ fun BlinkyScanner(
     onDeviceSelected: (BluetoothDevice, String?) -> Unit,
 ) {
     ScannerScreen(
-        title = stringResource(id = R.string.app_name),
+        title = stringResource(id = R.string.scanner_title),
         uuid = ParcelUuid(BlinkySpec.BLINKY_SERVICE_UUID),
         cancellable = false,
         onResult = { result ->
