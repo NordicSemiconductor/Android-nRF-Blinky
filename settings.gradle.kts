4d9e84e97c086df5c9f129e23b8b1ee513526210
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2024-12-02 11:51:32 +0100

    Comment out included libraries to use released versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,9 +28,14 @@ include(":blinky:spec")
 include(":blinky:ui")
 include(":blinky:ble")
 
-if (file("../Android-Common-Libraries").exists()) {
-    includeBuild("../Android-Common-Libraries")
-}
-if (file("../Android-BLE-Library").exists()) {
-    includeBuild("../Android-BLE-Library")
-}
+// Clone https://github.com/NordicPlayground/Android-Common-Libraries and
+// uncomment the following lines to modify source code of the Nordic Common library:
+//if (file("../Android-Common-Libraries").exists()) {
+//    includeBuild("../Android-Common-Libraries")
+//}
+
+// Clone https://github.com/NordicSemiconductor/Android-BLE-Library and
+// uncomment the following lines to modify source code of the BLE library:
+//if (file("../Android-BLE-Library").exists()) {
+//    includeBuild("../Android-BLE-Library")
+//}
