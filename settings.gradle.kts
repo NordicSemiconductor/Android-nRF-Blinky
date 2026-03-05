f13166240868ff9036ea1f41ea2e40bc19e24b66
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2026-03-05 13:27:05 +0100

    Nordic version catalog updated to 2026.03.00

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
         }
         // Link: https://github.com/NordicSemiconductor/Nordic-Version-Catalog
         create("nordic") {
-            from("no.nordicsemi.android:version-catalog:2026.02.00")
+            from("no.nordicsemi.android:version-catalog:2026.03.00")
         }
     }
 }
